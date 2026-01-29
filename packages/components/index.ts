c2ce589e3ddf134090fdb3bf1b9dcdf9e58af731
ShetePro <sheteprolin@gmail.com>
2026-01-29 11:31:25 +0800

    feat: 添加项目入口文件和组件库统一出口
    - 创建根目录 index.ts 统一导出所有模块
    - 创建 packages/components/index.ts 统一导出所有组件
    - 统一组件导出规范，支持命名导出和默认导出

---
+++ packages/components/index.ts
@@ -0,0 +1,39 @@
+// Button 组件
+export { default as ExportButton } from './Button/ExportButton.vue'
+export { default as ImportButton } from './Button/ImportButton.vue'
+export { default as PermissionButton } from './Button/PermissionButton.vue'
+
+// Dialog 组件
+export { BasicDialog } from './Dialog/index'
+
+// FileUpload 组件
+export { default as BasicUpload } from './FileUpload/BasicUpload.vue'
+
+// Form 组件
+export { BasicForm } from './Form/index'
+export { default as SearchForm } from './Form/src/SearchForm.vue'
+export { default as FormGroup } from './Form/src/FormGroup.vue'
+export { default as BasicFormItem } from './Form/src/BasicFormItem.vue'
+
+// Icon 组件
+export { Icon } from './Icon/index'
+
+// Pagination 组件
+export { default as BasicPagination } from './Pagination/BasicPagination.vue'
+
+// Radio 组件
+export { default as BasicRadio } from './Radio/BasicRadio.vue'
+
+// Select 组件
+export { default as BasicSelect } from './Select/BasicSelect.vue'
+export { default as ColorSelect } from './Select/ColorSelect.vue'
+
+// Table 组件
+export { default as BasicTable } from './Table/index'
+export { default as TableColumn } from './Table/src/TableColumn.vue'
+export { default as Column } from './Table/src/Column.vue'
+
+// 类型导出
+export type * from './Form/src/types'
+export type * from './Dialog/src/types'
+export type * from './Table/src/types'
